d1d8dc0f0ed940c5d7b613de8421a0275eba86f4
Karim Hamidou <khamidou@gmail.com>
2017-03-23 13:36:43 -0700

    Use the right version of minidump depending on the OS.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org/"
 gem "sinatra", "~> 1.4.7"
 gem "sentry_breakpad", "~> 0.1.10"
+gem "os", "~> 1.0.0"
--- app.rb
+++ app.rb
@@ -1,5 +1,40 @@
+require 'os'
 require 'sinatra'
+require 'sentry_breakpad'
+require 'raven'
+
 
 get '/' do
-  "Hello World!"
+  "heroku-breakpad-sentry is a simple breakpad server which forwards data to sentry. It's very easy to deploy to heroku."
+end
+
+
+post '/crashreport' do
+  unless params[:upload_file_minidump] and params[:upload_file_minidump][:filename]
+    halt 400, "Need to pass minidump upload"
+  end
+
+  # Electron sends us crash reports in the minidump format.
+  # We need to read them and run minidump_stackwalk to be
+  # able to see them in an intelligible format.
+  file = params[:upload_file_minidump][:tempfile]
+  tmpfile = Tempfile.new('minidump')
+
+  begin
+    tmpfile.write(file.read)
+
+    res = ''
+    if OS.mac?
+      res = `./minidump_stackwalk_mac #{tmpfile.path}`
+    elsif OS.linux? and OS.bits == 64
+      res = `./minidump_stackwalk_linux64 #{tmpfile.path}`
+    end
+
+    SentryBreakpad.send_from_string(res)
+  ensure
+    tmpfile.close
+    tmpfile.unlink
+  end
+
+  return 'ok'
 end
